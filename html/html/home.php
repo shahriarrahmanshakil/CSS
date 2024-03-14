05f98a89aab56f7b6e21a0a80927f7e33a1412dd
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-03-14 12:22:33 +0600

    html and css basic working is updated

---
+++ html/html/home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Table</title>
+</head>
+<body>
+	<h1>Table</h1>
+	<marquee>Finish your task within 6:30!</marquee>
+	<table border="2" style="width: 100%">
+  <tr>
+    <th colspan="2">Name</th>
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td rowspan="3" align="center">Jill</td>
+    <td>Smith</td>
+    <td>43</td>
+  </tr>
+  <tr>
+    
+    <td>Jackson</td>
+    <td rowspan="4"><img src="Bye.gif" width="90px"></td>
+  </tr>
+  <tr>
+    <td>dd</td>
+   
+  </tr>
+  <tr>
+    <td>Jackson</td>
+    <td>dd</td>
+   
+  </tr>
+  <tr>
+    <td>Jackson</td>
+    <td>dd</td>
+    
+  </tr>
+</table>
+</body>
+</html>
+
---
+++ html/html/table.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Table</title>
+</head>
+<body>
+
+<div> 
+	<div style="float: left;"> 
+	   <a href="">Menu</a>
+	</div>
+	<div style="padding-left:205px"> 
+	<iframe allowfullscreen="true" src="home.php" frameborder="2" width="600px" height="200px"></iframe>
+
+	</div>
+</div>
+	<h1>Table</h1>
+	<table border="2" style="width: 800px;"> 
+		<tr> 
+			<th>SL</th>
+			<th>Name</th>
+			<th colspan="2">Contact</th>
+			<th>Address</th>
+			<th>Salary</th>
+			<th>Country</th>
+		</tr>
+		<tr> 
+			<td>1</td>
+			<td>Farhana</td>
+			<td>43564</td>
+			<td>far@gmail.com</td>
+			<td>Dhaka</td>
+			<td>232323</td>
+			<td rowspan="6"><img src="pizza.jpg" width="500px"/></td>
+		</tr>
+		<tr> 
+			<td>2</td>
+			<td>Meem</td>
+			<td rowspan="3">43564</td>
+			<td colspan="3">far@gmail.com</td>
+		</tr>
+		<tr> 
+			<td>3</td>
+			<td>Farjana</td>
+			<td>far@gmail.com</td>
+			<td>Rampura</td>
+			<td>232323</td>
+		</tr>
+		<tr> 
+			<td>4</td>
+			<td>Tangila</td>
+			<td>far@gmail.com</td>
+			<td>Mirpur</td>
+			<td>232323</td>
+		</tr>
+		<tr> 
+			<td>5</td>
+			<td>Shakil</td>
+			<td>43564</td>
+			<td>far@gmail.com</td>
+			<td>Mirpur</td>
+			<td>232323</td>
+		</tr>
+		<tr> 
+			<td>6</td>
+			<td>Rakib</td>
+			<td>43564</td>
+			<td>far@gmail.com</td>
+			<td>Mirpur</td>
+			<td>232323</td>
+		</tr>
+	</table>
+</body>
+</html>
+
